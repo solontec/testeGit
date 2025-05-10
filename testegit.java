32ce34eb6aa8edf932ec921b28891a869ab78701
Micro <guisolon.tec@gmail.com>
2025-05-10 11:20:10 -0300

    Prontoa mudei agora

--- testegit.java
+++ testegit.java
@@ -1,7 +1,34 @@
-public class testegit {
+import java.util.Scanner;
 
+public class testegit {
     public static void main(String[] args) {
         
-        System.out.println("Loucuraaaa");
-    }
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Digite seu user");
+        String user = scanner.nextLine();
+
+        System.out.println("Digite sua senha");
+        String pass = scanner.nextLine();
+
+        boolean page = true;
+
+        while (page) {
+
+
+            if(user.equals("Guisolon") && pass.equals("1234")){
+                System.out.println("Login efetuado");
+                break;
+
+            }  else  {
+                System.out.println("User errado tente novamente");
+            }
+
+            scanner.close();
+        
+           }
+
+            
+        }
+            
+        
 }
