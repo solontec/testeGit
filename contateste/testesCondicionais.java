a67cd261d344358c96cca7e0dd10561fbae20fe6
Micro <guisolon.tec@gmail.com>
2025-05-10 12:33:49 -0300

    teste de for i

---
+++ contateste/testesCondicionais.java
@@ -0,0 +1,12 @@
+package contateste;
+
+public class testesCondicionais{
+    public static void main(String[] args){
+
+        for (int i = 0; i < 10; i++) {
+            System.out.println(i);
+        }
+
+        
+    }
+}
---
+++ contateste/uuuu.java
@@ -0,0 +1,10 @@
+package contateste;
+
+public class uuuu {
+    public static void main(String[] args) {
+        
+
+
+        System.out.println("Deu bom fdp");
+    }
+}
